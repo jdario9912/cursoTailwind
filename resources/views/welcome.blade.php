861f3c3cc267442e87ef9aba4344801ad8b50ade
Joel Dario <joeldario9912@gmail.com>
2021-12-25 17:10:46 -0300

    08Sizes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,36 +17,16 @@
     </style>
 </head>
 <body>
-<div class="container">
-
-    <table class="table mt-2 w-full border-separate lg:border-collapse table-fixed">
-        <thead>
-            <tr>
-                <th class="w-1/4">Pais</th>
-                <th class="w-1/4">Ciudad</th>
-                <th class="w-1/2">Descripcion</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-            <tr>
-                <td>Argentina</td>
-                <td>Buenos Aires</td>
-                <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quod, asperiores optio, libero omnis quibusdam beatae voluptates facilis delectus sapiente, voluptas officia amet dolorum quis assumenda eos! Voluptate, magni laudantium.</td>
-            </tr>
-            <tr>
-                <td>Colombia</td>
-                <td>Bogota</td>
-                <td>Doloribus mollitia inventore quaerat eos nisi dicta porro tempore, sunt expedita assumenda ratione repellendus eius aspernatur sapiente molestias consequatur. Et incidunt autem unde, cupiditate placeat blanditiis quas quo! Suscipit, optio.</td>
-            </tr>
-            <tr>
-                <td>España</td>
-                <td>Madrid</td>
-                <td>Sequi et, deserunt est dicta expedita beatae debitis ullam error, reiciendis ea vitae tempora dolor provident quia ad tenetur itaque illum inventore totam vero culpa illo suscipit quidem corrupti. Necessitatibus?</td>
-            </tr>
-        </tbody>
-    </table>
-
-</div>
+    <div class="bg-black pt-10 w-64 h-screen">
+        <div class="container text-green-400">
+            <ul>
+                <li><a href="">Link 4</a></li>
+                <li><a href="">Link 4</a></li>
+                <li><a href="">Link 4</a></li>
+                <li><a href="">Link 4</a></li>
+                <li><a href="">Link 4</a></li>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
