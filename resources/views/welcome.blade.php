512827e09fee2ca3cd1e6038495c51e69ff0049e
Joel Dario <joeldario9912@gmail.com>
2021-12-27 18:09:40 -0300

    09Layout box-content, display block, float, objet contain cover top bottom, scroll, absolute, relative, fixed, sticky

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,27 +6,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Curso Tailwind</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <style>
-        .imagen{
-            background-image: url("{{asset('img/field-g74c45bb4f_1920.jpg')}}");
-            height: 400px;
-            width: 400px;
-            margin-left: auto;
-            margin-right: auto;
-        }
-    </style>
 </head>
 <body>
-    <div class="bg-black pt-10 w-64 h-screen">
-        <div class="container text-green-400">
-            <ul>
-                <li><a href="">Link 4</a></li>
-                <li><a href="">Link 4</a></li>
-                <li><a href="">Link 4</a></li>
-                <li><a href="">Link 4</a></li>
-                <li><a href="">Link 4</a></li>
-            </ul>
-        </div>
+    <div class="container">
+        <h1 class="bg-gray-300 text-gray-500 font-bold sticky top-0">Titulo 1</h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quasi odit sint, quos recusandae enim aut doloribus quam. Nostrum delectus saepe dolorem architecto molestiae maiores veniam commodi, quibusdam aliquam atque.</p>
+        <p>Tempore officiis rem sequi. Id nulla quae mollitia. Similique sit ipsa ipsum fugit! Nihil facere eligendi eaque provident praesentium rem qui rerum veritatis iure? Iusto illum in excepturi assumenda voluptatibus!</p>
+        <p>Facilis quaerat animi, repellat nobis ipsa modi obcaecati provident possimus in et cum consequatur cumque voluptate! Enim laborum animi eos quaerat molestias consequuntur, sit cupiditate nulla repudiandae quasi exercitationem veritatis?</p>
+        <p>Tempore vitae iste magnam obcaecati eum iure doloribus et debitis doloremque unde nihil pariatur qui ad, architecto voluptates consectetur aliquid voluptate? Voluptatem molestias, quidem non omnis adipisci ipsum aliquam sequi!</p>
+        <p>Eius fugiat modi, dolore labore reprehenderit sit, architecto odio placeat iusto assumenda qui atque porro suscipit iure, quod omnis molestiae. Suscipit quas ipsam similique laudantium velit pariatur numquam odio minus!</p>
+        <p>Quaerat, hic nesciunt iusto ipsa quos cumque ab ea in sed beatae, repellat maxime qui deleniti neque perferendis dignissimos totam. Amet itaque sit inventore quisquam similique dolore cupiditate odit laudantium.</p>
+        <p>Doloribus sapiente commodi nam. Culpa, inventore autem similique cumque aliquam repudiandae tempora illo molestiae ipsum eius debitis dolor fugit accusamus explicabo sit repellendus. Corrupti nihil dolorum doloribus qui magni ab.</p>
+        <p>Eligendi asperiores perferendis deserunt odit praesentium exercitationem est sapiente et obcaecati recusandae pariatur, doloremque reprehenderit ab quas quod! Iste a nulla tempore odit dolorum nemo officiis iure repellendus culpa expedita.</p>
+        
+        
+        <h1 class="bg-gray-300 text-gray-500 font-bold sticky top-0">Titulo 2</h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quasi odit sint, quos recusandae enim aut doloribus quam. Nostrum delectus saepe dolorem architecto molestiae maiores veniam commodi, quibusdam aliquam atque.</p>
+        <p>Tempore officiis rem sequi. Id nulla quae mollitia. Similique sit ipsa ipsum fugit! Nihil facere eligendi eaque provident praesentium rem qui rerum veritatis iure? Iusto illum in excepturi assumenda voluptatibus!</p>
+        <p>Facilis quaerat animi, repellat nobis ipsa modi obcaecati provident possimus in et cum consequatur cumque voluptate! Enim laborum animi eos quaerat molestias consequuntur, sit cupiditate nulla repudiandae quasi exercitationem veritatis?</p>
+        <p>Tempore vitae iste magnam obcaecati eum iure doloribus et debitis doloremque unde nihil pariatur qui ad, architecto voluptates consectetur aliquid voluptate? Voluptatem molestias, quidem non omnis adipisci ipsum aliquam sequi!</p>
+        <p>Eius fugiat modi, dolore labore reprehenderit sit, architecto odio placeat iusto assumenda qui atque porro suscipit iure, quod omnis molestiae. Suscipit quas ipsam similique laudantium velit pariatur numquam odio minus!</p>
+        <p>Quaerat, hic nesciunt iusto ipsa quos cumque ab ea in sed beatae, repellat maxime qui deleniti neque perferendis dignissimos totam. Amet itaque sit inventore quisquam similique dolore cupiditate odit laudantium.</p>
+        <p>Doloribus sapiente commodi nam. Culpa, inventore autem similique cumque aliquam repudiandae tempora illo molestiae ipsum eius debitis dolor fugit accusamus explicabo sit repellendus. Corrupti nihil dolorum doloribus qui magni ab.</p>
+        <p>Eligendi asperiores perferendis deserunt odit praesentium exercitationem est sapiente et obcaecati recusandae pariatur, doloremque reprehenderit ab quas quod! Iste a nulla tempore odit dolorum nemo officiis iure repellendus culpa expedita.</p>
+        
+        
+        <h1 class="bg-gray-300 text-gray-500 font-bold sticky top-0">Titulo 3</h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quasi odit sint, quos recusandae enim aut doloribus quam. Nostrum delectus saepe dolorem architecto molestiae maiores veniam commodi, quibusdam aliquam atque.</p>
+        <p>Tempore officiis rem sequi. Id nulla quae mollitia. Similique sit ipsa ipsum fugit! Nihil facere eligendi eaque provident praesentium rem qui rerum veritatis iure? Iusto illum in excepturi assumenda voluptatibus!</p>
+        <p>Facilis quaerat animi, repellat nobis ipsa modi obcaecati provident possimus in et cum consequatur cumque voluptate! Enim laborum animi eos quaerat molestias consequuntur, sit cupiditate nulla repudiandae quasi exercitationem veritatis?</p>
+        <p>Tempore vitae iste magnam obcaecati eum iure doloribus et debitis doloremque unde nihil pariatur qui ad, architecto voluptates consectetur aliquid voluptate? Voluptatem molestias, quidem non omnis adipisci ipsum aliquam sequi!</p>
+        <p>Eius fugiat modi, dolore labore reprehenderit sit, architecto odio placeat iusto assumenda qui atque porro suscipit iure, quod omnis molestiae. Suscipit quas ipsam similique laudantium velit pariatur numquam odio minus!</p>
+        <p>Quaerat, hic nesciunt iusto ipsa quos cumque ab ea in sed beatae, repellat maxime qui deleniti neque perferendis dignissimos totam. Amet itaque sit inventore quisquam similique dolore cupiditate odit laudantium.</p>
+        <p>Doloribus sapiente commodi nam. Culpa, inventore autem similique cumque aliquam repudiandae tempora illo molestiae ipsum eius debitis dolor fugit accusamus explicabo sit repellendus. Corrupti nihil dolorum doloribus qui magni ab.</p>
+        <p>Eligendi asperiores perferendis deserunt odit praesentium exercitationem est sapiente et obcaecati recusandae pariatur, doloremque reprehenderit ab quas quod! Iste a nulla tempore odit dolorum nemo officiis iure repellendus culpa expedita.</p>
     </div>
 </body>
 </html>
